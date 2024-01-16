bd0a86a6e53299c34d811805b378796a63b130d7
Aman <91665648+Amnx404@users.noreply.github.com>
2024-01-16 01:44:41 -0500

    Revert "Revert "enhanced functionality and filterable""
    This reverts commit 9a1e34ffc27de817c71b2ed1daa24796d344d393.

--- src/ContestDisplay.js
+++ src/ContestDisplay.js
@@ -1,21 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
+import { useTable, usePagination } from 'react-table';
 import { Link } from 'react-router-dom';
 import { database } from './firebase';
 import { ref, onValue } from 'firebase/database';
 import './ContestDisplay.css';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+
+
 
 const ContestDisplay = () => {
-  const [contestData, setContestData] = useState({});
-  const [currentPage, setCurrentPage] = useState(1);
-  const [entriesPerPage] = useState(10); // Number of entries per page
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
-  // Fetch contest data from Firebase
   useEffect(() => {
     const contestRef = ref(database, 'CONTESTS');
     const unsubscribe = onValue(contestRef, (snapshot) => {
-      setContestData(snapshot.val() || {});
+      const snapshotData = snapshot.val() || {};
+      const formattedData = Object.entries(snapshotData).map(([id, entry]) => ({
+        id,
+        ...entry
+      }));
+      setData(formattedData);
       setLoading(false);
     }, (error) => {
       setError('Failed to load data');
@@ -25,7 +31,102 @@ const ContestDisplay = () => {
     return () => unsubscribe();
   }, []);
 
-  // Display loading or error message
+  const columns = useMemo(() => {
+    const manualColumns = [
+      {
+        Header: 'ContestID',
+        accessor: 'ContestID'
+      },
+      {
+        Header: 'End Time',
+        accessor: 'EndTime'
+      },
+      {
+        Header: 'Ended',
+        accessor: 'ended',
+        Cell: ({ value }) => typeof value === 'boolean' ? (value ? 'True' : 'False') : value
+      },
+      {
+        Header: 'Contestants',
+        accessor: 'Contestants',
+        Cell: ({ row }) => <Link to={`/contests/${row.original.ContestID}`}>View Contestants</Link>
+      },
+      {
+        Header: 'FirstPrize',
+        accessor: 'FirstPrize'
+      },
+      {
+        Header: 'SecondPrize',
+        accessor: 'SecondPrize'
+      },
+      {
+        Header: 'ThirdPrize',
+        accessor: 'ThirdPrize'
+      },
+      {
+        Header: 'FourthPrize',
+        accessor: 'FourthPrize'
+      },
+      {
+        Header: 'FifthPrize',
+        accessor: 'FifthPrize'
+      },
+      // ... other manually defined columns ...
+    ];
+
+    const additionalColumns = data.length > 0 
+      ? Object.keys(data[0]).reduce((acc, key) => {
+          if (!manualColumns.find(col => col.accessor === key)) {
+            acc.push({
+              Header: key.charAt(0).toUpperCase() + key.slice(1),
+              accessor: key,
+              Cell: ({ value }) => {
+                if (value && typeof value === 'object' && !(value instanceof Date)) {
+                  const stringValue = JSON.stringify(value, null);
+                  //return span with class name cell-truncate
+                  if (stringValue.length > 30){
+                    return <span className="cell-truncate" title={stringValue}>{stringValue.substring(0, 30) + "..."}</span>;
+                  }
+                  return <span className="cell-truncate" title={stringValue}>{stringValue}</span>;
+
+                }
+                //if boolean value then return true or false
+                if (typeof value === 'boolean') {
+                  return value ? 'True' : 'False';
+                }
+                
+                return value;
+              }
+            });
+          }
+          return acc;
+      }, [])
+      : [];
+
+  // This is where you combine manualColumns and additionalColumns
+  return [...manualColumns, ...additionalColumns];
+}, [data]); // Dependency array for useMemo
+
+  const tableInstance = useTable(
+    { columns, data },
+    usePagination
+  );
+
+  // Destructure the required properties and methods from the table instance
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    nextPage,
+    previousPage,
+    state: { pageIndex },
+  } = tableInstance;
+
   if (loading) {
     return <p>Loading contest data...</p>;
   }
@@ -34,50 +135,49 @@ const ContestDisplay = () => {
     return <p>{error}</p>;
   }
 
-  if (!contestData || Object.keys(contestData).length === 0) {
+  if (data.length === 0) {
     return <p>No contest data available</p>;
   }
-
-  // Generate table headers
-  const tableHeaders = ['Contestants', ...new Set(Object.keys(contestData).flatMap(key => Object.keys(contestData[key]).filter(header => header !== 'Contestants')))];
-  
-  // Pagination logic
-  const indexOfLastEntry = currentPage * entriesPerPage;
-  const indexOfFirstEntry = indexOfLastEntry - entriesPerPage;
-  const currentEntries = Object.entries(contestData).slice(indexOfFirstEntry, indexOfLastEntry);
-
-  // Handle page change
-  const handlePageChange = (newPage) => {
-    setCurrentPage(newPage);
-  };
 
   return (
     <div>
       <h2>Contest Details</h2>
-      <div>
-        <table className="contest-table">
-          <thead>
-            <tr>
-              {tableHeaders.map(header => <th key={header}>{header}</th>)}
+      <table {...getTableProps()} className="contest-table">
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
             </tr>
-          </thead>
-          <tbody>
-            {currentEntries.map(([id, entry]) => (
-              <tr key={id}>
-                {tableHeaders.map(header => (
-                  <td key={`${id}-${header}`}>
-                    {header === 'Contestants' ? <Link to={`/contests/${id}`}>View Contestants</Link> : entry[header]}
-                  </td>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map(row => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => (
+                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
                 ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="pagination">
-          {Array.from({ length: Math.ceil(Object.keys(contestData).length / entriesPerPage) }, (_, index) => (
-            <button key={index} onClick={() => handlePageChange(index + 1)} className={currentPage === index + 1 ? 'active' : ''}>{index + 1}</button>
-          ))}
-        </div>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
       </div>
     </div>
   );
--- src/UsersDisplay.js
+++ src/UsersDisplay.js
@@ -1,20 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
+import { useTable, usePagination } from 'react-table';
 import { Link } from 'react-router-dom';
 import { database } from './firebase';
 import { ref, onValue } from 'firebase/database';
 import './UserDisplay.css';
 
 const UserDisplay = () => {
-  const [userData, setUserData] = useState({});
-  const [currentPage, setCurrentPage] = useState(1);
-  const [entriesPerPage] = useState(10);
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
   useEffect(() => {
     const userRef = ref(database, 'USERS');
     const unsubscribe = onValue(userRef, (snapshot) => {
-      setUserData(snapshot.val() || {});
+      const snapshotData = snapshot.val() || {};
+      const formattedData = Object.entries(snapshotData).map(([id, entry]) => ({
+        id,
+        ...entry
+      }));
+      setData(formattedData);
       setLoading(false);
     }, (error) => {
       setError('Failed to load data');
@@ -24,63 +28,132 @@ const UserDisplay = () => {
     return () => unsubscribe();
   }, []);
 
+
+
+  const columns = useMemo(() => {
+    if (data.length === 0) {
+      return [];
+    }
+
+    // Manually defined columns
+    const manualColumns = [
+      {
+        Header: 'User ID',
+        accessor: 'id',
+      },
+      {
+        Header: 'myContestsJoined',
+        accessor: 'myContestsJoined',
+        Cell: ({ row }) => <Link to={`/users/${row.original.id}/myContestsJoined`}>View Contests</Link>,
+      },
+      {
+        Header: 'myContestsCompleted',
+        accessor: 'myContestsCompleted',
+        Cell: ({ row }) => <Link to={`/users/${row.original.id}/myContestsCompleted`}>View Contests</Link>,
+      },
+    ];
+
+
+
+    
+    // Extract keys from data for additional columns
+    const additionalColumns = data.length > 0 
+      ? Object.keys(data[0]).reduce((acc, key) => {
+          if (!manualColumns.find(col => col.accessor === key)) {
+            acc.push({
+              Header: key.charAt(0).toUpperCase() + key.slice(1),
+              accessor: key,
+              Cell: ({ value }) => {
+                if (value && typeof value === 'object' && !(value instanceof Date)) {
+                  const stringValue = JSON.stringify(value, null);
+                  //return span with class name cell-truncate
+                  if (stringValue.length > 30){
+                    return <span className="cell-truncate" title={stringValue}>{stringValue.substring(0, 30) + "..."}</span>;
+                  }
+                  return <span className="cell-truncate" title={stringValue}>{stringValue}</span>;
+
+                }
+                //if boolean value then return true or false
+                if (typeof value === 'boolean') {
+                  return value ? 'True' : 'False';
+                }
+                
+                return value;
+              }
+            });
+          }
+          return acc;
+      }, [])
+      : [];
+
+
+    // Combine manually defined columns and additional columns
+    return [...manualColumns, ...additionalColumns];
+  }, [data]);
+
+  const tableInstance = useTable(
+    { columns, data, initialState: { pageIndex: 0 } },
+    usePagination
+  );
+
+  // Destructure the required properties and methods from the table instance
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    nextPage,
+    previousPage,
+    state: { pageIndex },
+  } = tableInstance;
+
   if (loading) return <p>Loading user data...</p>;
   if (error) return <p>{error}</p>;
-  if (!userData || Object.keys(userData).length === 0) return <p>No user data available</p>;
-
-  const handleNestedData = (data) => {
-    if (typeof data === 'object') {
-      return JSON.stringify(data, null, 2); // Converts object to a formatted string
-    }
-    return data;
-  };
-
-  // Update here to include myContestsJoined
-  const tableHeaders = Object.keys(userData).reduce((acc, key) => {
-    const userKeys = Object.keys(userData[key]);
-    userKeys.forEach(k => {
-      if (!acc.includes(k)) acc.push(k);
-    });
-    return acc;
-  }, []);
-
-  const indexOfLastEntry = currentPage * entriesPerPage;
-  const indexOfFirstEntry = indexOfLastEntry - entriesPerPage;
-  const currentEntries = Object.entries(userData).slice(indexOfFirstEntry, indexOfLastEntry);
-
-  const handlePageChange = (newPage) => setCurrentPage(newPage);
+  if (data.length === 0) return <p>No user data available</p>;
 
   return (
     <div>
       <h2>User Details</h2>
-      <table className="user-table">
+      <table {...getTableProps()} className="user-table">
         <thead>
-          <tr>
-            {tableHeaders.map(header => <th key={header}>{header}</th>)}
-          </tr>
-        </thead>
-        <tbody>
-          {currentEntries.map(([id, entry]) => (
-            <tr key={id}>
-              {tableHeaders.map(header => (
-                <td key={`${id}-${header}`}>
-                  {header === 'myContestsCompleted' || header === 'myContestsJoined' ? (
-                    <Link to={`/users/${id}/${header}`}>{`View ${header}`}</Link>
-                  ) : (
-                    handleNestedData(entry[header])
-                  )}
-                </td>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
               ))}
             </tr>
           ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map(row => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => (
+                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                ))}
+              </tr>
+            );
+          })}
         </tbody>
       </table>
       <div className="pagination">
-        {Array.from({ length: Math.ceil(Object.keys(userData).length / entriesPerPage) }, (_, index) => (
-          <button key={index} onClick={() => handlePageChange(index + 1)} className={currentPage === index + 1 ? 'active' : ''}>
-            {index + 1}
-          </button>
-        ))}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+        </span>
       </div>
     </div>
   );
